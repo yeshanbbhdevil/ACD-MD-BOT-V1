837bb25088ab95eda0a57b1137883c1dac51e50c
CRAZY-CHUTTA <yeshanbasnayake20071001@gmail.com>
2024-12-01 12:48:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ndNnALSR#msNJIZ-N2w5qTqW6mQgoWzQNVEC8PlBh3joeGlXlgns",  //මෙතනට ඔයාගෙ සෙශන් කෝඩ් එක දාන්න
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/Qp22bZ1/IMG-20241129-WA0428.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/hLGPJ0W/IMG-20241201-WA0167.jpg",
 ALIVE_MSG: process.env.ALIVE_IMG || "*HEY DEAR* ${pushname}\n *I Am AlIVE NOW....! 🖐🏻*",
 SUDO_NB: process.env.SUDO_NB || "94701219309",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
